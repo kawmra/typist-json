c3647b8ef2cf4bf609104e953141a13738f7e776
kawmra <m.kawmra@gmail.com>
2021-03-06 22:22:30 +0900

    add Rollup and update build process

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  input: 'build/index.js',
+  output: [
+    {
+      file: 'lib/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'lib/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+};
